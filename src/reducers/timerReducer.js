579bdb3d7d063c28b70b8c449044ac9aa5bb0088
Peter Tan <pxhtan@gmail.com>
2018-12-16 12:00:28 -0800

    Initialize default state, reducer for timer

---
+++ src/reducers/timerReducer.js
@@ -0,0 +1,39 @@
+import { actionTypes } from "../actions/actionTypes";
+import converters from '../Util/timeConverts';
+
+const { convertSecs } = converters;
+
+
+export default function (state = {
+  display: {hrs: 0, mins: 0, secs: 0},
+  countSecs: 0,
+  isPause: false,
+  isInTimer: false,
+}, action) {
+  switch (action.type) {
+    case actionTypes.START:
+      const newState = state;
+      state.countSecs = action.payload;
+      state.isInTimer = true;
+      state.display = convertSecs(action.payload);
+
+      return Object.assign({}, state, newState);
+    case actionTypes.PAUSE:
+      const newState = state;
+      state.isPause = true;
+
+      return Object.assign({}, state, newState)
+    case actionTypes.RESUME:
+      const newState = state;
+      state.isPause = false;
+
+      return Object.assign({}, state, newState)
+    case actionTypes.RESET:
+      const newState = state;
+      state.isInTimer = false;
+
+      return Object.assign({}, state, newState)
+    default:
+      return state;
+   }
+}
