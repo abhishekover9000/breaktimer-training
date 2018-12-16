939f69c0808855a6e5b9c2914a7a831313a5abb6
Varun Koppana <varunkoppana@Varuns-MacBook-Pro.local>
2018-12-16 11:30:16 -0800

    creation of config componenet

--- src/App.js
+++ src/App.js
@@ -7,12 +7,32 @@ import rootReducer from "./reducers/reducers";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import ConfigTimer from "./components/configTimer";
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
+  state = {
+    configPage: false
+  };
+addItemToggle = () => {
+    this.setState({ configPage: true });
+  };
+
   render() {
     return (
       <Provider store={createStore(rootReducer, applyMiddleware(thunk))}>
-        <div> lets add things </div>
+        <Button
+            variant="contained"
+            color="primary"
+            onClick={this.addItemToggle}
+          >
+            {" "}
+            Config{" "}
+          </Button>
+        
+        {this.state.configPage ? (
+          <ConfigTimer/>
+        ) : null}
       </Provider>
     );
   }
---
+++ src/components/configTimer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ConfigTimer extends Component{
+    
+render(){
+    return(
+        <div>
+            config timer
+        </div>
+    )
+}
+}
+export default ConfigTimer;
