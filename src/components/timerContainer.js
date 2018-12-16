d9d336b3f796e3862ef2ad54e74ccb3469a4b67e
Varun Koppana <varunkoppana@Varuns-MacBook-Pro.local>
2018-12-16 15:24:40 -0800

    load buttom

--- src/components/timerContainer.js
+++ src/components/timerContainer.js
@@ -14,64 +14,64 @@ class TimerContainer extends Component {
         secs: '',
         interval: ''
     };
-handleHours = (e) => {
+    handleHours = (e) => {
 
-    this.setState({ hrs: Number(e.target.value * 3600) });
-    const hsecs = e.target.value * 3600;
-    console.log(hsecs);
+        this.setState({ hrs: Number(e.target.value * 3600) });
+        const hsecs = e.target.value * 3600;
+        console.log(hsecs);
 
-  };
-  handleMinutes = e => {
-    this.setState({ mins: Number(e.target.value * 60) });
-    console.log("seconds is ", e.target.value * 60);
+    };
+    handleMinutes = e => {
+        this.setState({ mins: Number(e.target.value * 60) });
+        console.log("seconds is ", e.target.value * 60);
 
-  };
-  handleSeconds = e => {
-    this.setState({ secs: Number(e.target.value) });
-    console.log(e.target.value);
-  };
-  handleStart = () => {
-    // Use start action
-    const { start } = this.props;
-    const total = Number(this.state.hrs + this.state.mins + this.state.secs);
-    start(total);
-    const interval = setInterval(() => {
-      const { timerInfo } = this.props;
-      this.handleDecrement(timerInfo.countSecs);
-    }, 1000);
+    };
+    handleSeconds = e => {
+        this.setState({ secs: Number(e.target.value) });
+        console.log(e.target.value);
+    };
+    handleStart = () => {
+        // Use start action
+        const { start } = this.props;
+        const total = Number(this.state.hrs + this.state.mins + this.state.secs);
+        start(total);
+        const interval = setInterval(() => {
+            const { timerInfo } = this.props;
+            this.handleDecrement(timerInfo.countSecs);
+        }, 1000);
 
-    this.setState({interval});
-  }
-  handleRestart = () => {
-    // Use reset action
-    // this.setState({ isInTimer: false });
-    const { reset } = this.props;
-    clearInterval(this.state.interval);
-    reset()
-  }
-  handlePause = () =>{
- 
-    const { pause } = this.props;
-    pause();
-  }
-  handleResume = () =>{
- 
-    const { resume } = this.props;
-    resume();
-  }
-  handleDecrement = () => {
-    const { timerInfo, decrement } = this.props;
-    const {isPause} = timerInfo
-
-    if (!isPause) {
-      decrement(timerInfo.countSecs);
+        this.setState({ interval });
     }
-  }
+    handleRestart = () => {
+        // Use reset action
+        // this.setState({ isInTimer: false });
+        const { reset } = this.props;
+        clearInterval(this.state.interval);
+        reset()
+    }
+    handlePause = () => {
 
-  shouldComponentUpdate(nextProps, nextState, nextContext) {
-    console.log(nextProps);
-    return true;
-  }
+        const { pause } = this.props;
+        pause();
+    }
+    handleResume = () => {
+
+        const { resume } = this.props;
+        resume();
+    }
+    handleDecrement = () => {
+        const { timerInfo, decrement } = this.props;
+        const { isPause } = timerInfo
+
+        if (!isPause) {
+            decrement(timerInfo.countSecs);
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log(nextProps);
+        return true;
+    }
     configDisplay = () => {
         this.setState({ configPage: true, timerPage: false });
     };
@@ -84,9 +84,12 @@ handleHours = (e) => {
     timerDisplay = () => {
         this.setState({ configPage: false, timerPage: true });
     }
+    load =() => {
+        console.log("load functionality");
+    }
     render() {
-          const { timerInfo } = this.props;
-    const { hrs, mins, secs } = timerInfo.display;
+        const { timerInfo } = this.props;
+        const { hrs, mins, secs } = timerInfo.display;
         return (
             <div>
                 <div className="row">
@@ -107,65 +110,77 @@ handleHours = (e) => {
                         Config{" "}
                     </Button>
                 </div>
-           <div>
-        {!timerInfo.isInTimer ?
-          <React.Fragment>
-            <TextField
+               
+                {this.state.timerPage?(
+                <div>
+                     {this.props.config.name !== "" ? (
+                         <div>
+                     <Button
+                     variant="contained"
+                     color="primary"
+                     onClick={this.load}
+                 >
+                     {" "}
+                     Load{" "}{this.props.config.name}
+                 </Button>
+                 </div>
+                ) : <div>No Config</div>}
+                    {!timerInfo.isInTimer ?
+                        <React.Fragment>
+                            <TextField
 
-              label="Hours"
-              value={this.state.value}
-              onChange={this.handleHours}
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
+                                label="Hours"
+                                value={this.state.value}
+                                onChange={this.handleHours}
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                margin="normal"
+                            />
 
-            <TextField
+                            <TextField
 
-              label="Minutes"
-              value={this.state.value}
-              onChange={this.handleMinutes}
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
-            <TextField
+                                label="Minutes"
+                                value={this.state.value}
+                                onChange={this.handleMinutes}
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                margin="normal"
+                            />
+                            <TextField
 
-              label="Seconds"
-              value={this.state.value}
-              onChange={this.handleSeconds}
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
-            <br />
-            {this.state.hrs > 0 || this.state.mins > 0 || this.state.secs > 0 ?
-              <React.Fragment><Button color="primary" onClick={this.handleStart} >
-                Start Timer
+                                label="Seconds"
+                                value={this.state.value}
+                                onChange={this.handleSeconds}
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                margin="normal"
+                            />
+                            <br />
+                            {this.state.hrs > 0 || this.state.mins > 0 || this.state.secs > 0 ?
+                                <React.Fragment><Button color="primary" onClick={this.handleStart} >
+                                    Start Timer
       </Button> </React.Fragment> : <p><b>Enter values to start timer</b></p>
 
-            }
-          </React.Fragment>
-          :
-          <React.Fragment>
-            <p>{`Remaining Hours: ${hrs}`}</p>
-            <p>{`Minutes: ${mins}`}</p>
-            <p>{`Seconds: ${secs}`}</p>
-            <Button color="primary" onClick ={this.handlePause}>Pause</Button>
-            <Button color="primary" onClick ={this.handleResume}>Resume</Button>
-            <Button color="primary" onClick={this.handleRestart}>Restart</Button>
-          </React.Fragment>
-        }
-      </div>
-                {this.props.config ? (
-                    <div>{this.props.config.name},{this.props.config.time}</div>
-                ) : null}
+                            }
+                        </React.Fragment>
+                        :
+                        <React.Fragment>
+                            <p>{`Remaining Hours: ${hrs}`}</p>
+                            <p>{`Minutes: ${mins}`}</p>
+                            <p>{`Seconds: ${secs}`}</p>
+                            <Button color="primary" onClick={this.handlePause}>Pause</Button>
+                            <Button color="primary" onClick={this.handleResume}>Resume</Button>
+                            <Button color="primary" onClick={this.handleRestart}>Restart</Button>
+                        </React.Fragment>
+                    }
+                </div>):null}
+                
                 {this.state.configPage ? (
                     <ConfigTimer updateConfig={this.updateConfig} />
                 ) : null}
@@ -175,17 +190,17 @@ handleHours = (e) => {
 
 }
 const mapStateToProps = (state) => {
-  return {
-    timerInfo: state.timerInfo,
-    config: state.config
-  }
+    return {
+        timerInfo: state.timerInfo,
+        config: state.config
+    }
 }
 export default connect(mapStateToProps, {
-  start,
-  pause,
-  resume,
-  reset,
-  decrement
+    start,
+    pause,
+    resume,
+    reset,
+    decrement
 }
 )(TimerContainer);
 
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,7 +1,5 @@
 import { combineReducers } from "redux";
-import todo from "./todoReducer.js";
-import weather from "./weatherReducer.js";
 import config from "./configReducer.js";
 import timerInfo from "./timerReducer";
 // automagical mapping
-export default combineReducers({ weather, todo, config,timerInfo });
+export default combineReducers({ config,timerInfo });
